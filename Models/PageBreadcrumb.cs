ddd5ea71434ae135a9a995d25d09e6aee1239064
FernandoAMartinez <fernandomartinez0295@outlook.com>
2024-02-14 03:21:34 -0300

    Initial blazor web app with iframe function

---
+++ Models/PageBreadcrumb.cs
@@ -0,0 +1,7 @@
+    namespace Wikifound.Models;
+    public class PageBreadcrumb
+    {
+        public string Url {get;set;}
+        public string Page {get; set;}
+        public string Active {get;set;} = "";
+    }
