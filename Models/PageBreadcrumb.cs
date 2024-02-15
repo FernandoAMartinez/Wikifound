466b5f6c220c561d596b09aad32dffd10ac57667
FernandoAMartinez <fernandomartinez0295@outlook.com>
2024-02-15 15:16:48 -0300

    feat: query string route parameters from home to play

--- Models/PageBreadcrumb.cs
+++ Models/PageBreadcrumb.cs
@@ -1,7 +1,9 @@
     namespace Wikifound.Models;
     public class PageBreadcrumb
     {
-        public string Url {get;set;}
-        public string Page {get; set;}
-        public string Active {get;set;} = "";
+        public string? Url {get;set;}
+        public string? Page {get; set;}
+        public bool Active {get;set;}
+        public int Attemp {get;set;}
+
     }
